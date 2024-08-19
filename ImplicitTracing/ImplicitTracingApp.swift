e4cad3830e5c713e7982e53f32177b3e6b47dec4
Adellar Irankunda <addyirankunda@gmail.com>
2024-08-18 18:51:37 -0700

    Initial Commit

---
+++ ImplicitTracing/ImplicitTracingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplicitTracingApp.swift
+//  ImplicitTracing
+//
+//  Created by Adellar Irankunda on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ImplicitTracingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
